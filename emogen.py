253a21a94de3b3cd364acde910fd9b80f20eef8f
nfisherman <ca.lehman05@gmail.com>
2024-04-03 23:11:00 -0500

    added constructor

--- emogen.py
+++ emogen.py
@@ -4,8 +4,9 @@ import argparse
 
 
 class EmoGen(object):
-    def __init__(self):
-        pass
+    def __init__(self, data_source):
+        self.data_source = data_source
+
 
 
 if __name__ == '__main__':
