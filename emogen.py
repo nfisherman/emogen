13a5fc06a081c1a4b17bcf9b35be14b8fe361aad
nfisherman <ca.lehman05@gmail.com>
2024-04-05 03:38:25 +0000

    added l33t stuff

--- emogen.py
+++ emogen.py
@@ -4,7 +4,7 @@ import argparse
 from mimetypes import guess_type
 from pathlib import Path
 from typing import Final
-from random import randrange
+from random import randrange, choice
 
 
 class EmoGen:
@@ -51,24 +51,32 @@ class EmoGen:
                         seperated_line: list[str] = line.split('|')
                         seperated_line[-1] = seperated_line[-1].strip(' \t\n\r')
 
-                        self.data = seperated_line
+                        self.data.append(seperated_line)
 
         if len(self.data) == 0:
             self.data = None
 
         return self.data is not None
 
-    def generate(self) -> str:
-        result: str = str()
+    def generate(self, prefix_chance: int=2, l33t_chance: int=2) -> str:
+        result: str = ''
+        
+        end_caps: str = None
+        if prefix_chance <= 0 or randrange(prefix_chance): # (default) 50% chance of prefix & suffix
+            end_caps = choice(self.PREFIX_SUFFIX)
+            result += end_caps[0]
 
-        prefix: str = None
-        if randrange(2):
-            prefix = randrange(len(self.PREFIX_SUFFIX))
-            str += self.PREFIX_SUFFIX[prefix][0]
+        user: list[str] = choice(self.data)
 
-
+        if l33t_chance <= 0 or randrange(l33t_chance): # (default) 50% chance of l33t
+            for i in range(len(user)):
+                for j in range(len(user[i]))[1:]:
+                    if user[i][j] in self.LETTER_NUMBER.keys() and (l33t_chance <= 0 or randrange(l33t_chance)):
+                        user[i][j] = self.LETTER_NUMBER[user[i][j].upper()]
+                        print(user[i])
+                        break
 
 
 if __name__ == '__main__':
     emo_gen: EmoGen = EmoGen(Path('sources'))
-    print(emo_gen)
+    emo_gen.generate()
